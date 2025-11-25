58804560d7640474ef5a92b632b2c3fbdd0f3301
dyymon <davkovar@email.cz>
2025-11-25 22:25:16 +0100

    adding new homework

---
+++ homeworks/compulsory_homeworks/hw_02/sort.py
@@ -0,0 +1,34 @@
+
+def find_minimum(numbers: list[int]) -> float:
+    """
+    Find the smallest number in the given list of numbers.
+    :param numbers: list of numbers
+    :return: smallest number
+    """
+    minimum = numbers[0]
+
+    return minimum
+
+
+def find_maximum(numbers: list[int]) -> float:
+    """
+    Find the highest number in the given list of numbers.
+    :param numbers: list of numbers
+    :return: highest number
+    """
+    maximum = numbers[0]
+
+    return maximum
+
+def sort_numbers(numbers: list) -> list:
+    """
+    sort a list of numbers from lowest to highest
+    :param numbers: list of numbers to sort
+    :return: list of sorted numbers
+    """
+
+    return numbers
+
+
+if __name__ == "__main__":
+    numbers = [1, 2, 12, 4, 32, 6, 75, 68, 9, 110]
